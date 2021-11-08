e4a6d227c417aa95adbc1f8f8e1e8693acde87aa
Kwame Osae Afari <kayfari@MacBook-Pro.local>
2021-11-08 13:18:10 +0000

    added an infowindow to markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -9,6 +9,8 @@ interface Mappable {
     lng: number;
   };
 }
+
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -23,12 +25,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable ): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there'
+      });
+
+      infoWindow.open(this.googleMap, marker)
     });
   }
 
